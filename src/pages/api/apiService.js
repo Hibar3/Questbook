2a9defa3c53b612eccd54a747ac690200aaec3ca
Hibari <hibar3chan@gmail.com>
2021-06-04 02:00:33 +0800

    fix api service

--- src/pages/api/apiService.js
+++ src/pages/api/apiService.js
@@ -1,6 +1,6 @@
 
-export default getTask(){
-    fetch("http://localhost:4001/task" )
+export const getTask=()=>{
+    fetch("http://localhost:4001/task")
 }
 
 
@@ -8,10 +8,11 @@ export default getTask(){
 
 
 
+
+export default getTask
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 // export default (req, res) => {
 //     res.statusCode = 200
 //     res.json({ name: 'John Doe' })
 //   }
-  
